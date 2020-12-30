35eee8b39d1125869d1a467761d977203728f3aa
AlexSh <hvit-hest@yandex.ru>
2020-12-30 10:55:41 +0300

    minor changes on DuckDetailsPage

--- src/test/java/org/courses/pages/duckdetailspage/DuckDetailsPage.java
+++ src/test/java/org/courses/pages/duckdetailspage/DuckDetailsPage.java
@@ -7,8 +7,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-import java.util.List;
-
 public class DuckDetailsPage {
     private WebDriver driverHere;
 
@@ -32,29 +30,4 @@ public class DuckDetailsPage {
     public DuckDetailsSection getDuckDetailsSection() {
         return new DuckDetailsSection(driverHere.findElement(By.cssSelector("#box-product")));
     }
-
-    /*@FindBy(css = "#box-product.box .regular-price, #box-product.box .price")
-    private WebElement regularPriceWE;
-
-    *//*It could be absent so we are using the list to escape exception and only first element will be taken
-    to get data*//*
-    @FindBy(css = "#box-product.box .campaign-price")
-    private List<WebElement> campaignPriceWEs;
-
-
-    public String getRegularPrice() {
-        return regularPriceWE.getText();
-    }
-
-    public String getRegularPriceColor() {
-        return regularPriceWE.getCssValue("color");
-    }
-
-    public String getRegularPriceDecoration() {
-        return regularPriceWE.getCssValue("text-decoration");
-    }
-
-    public String getCampaignPriceWE() {
-
-    }*/
 }
